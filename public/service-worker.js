d93e525d9dfd2d77b7bf75d1c5c5539c83da2582
maxjf1 <maxwellsouzacarvalho@gmail.com>
2018-03-27 17:07:03 -0300

    debug

---
+++ public/service-worker.js
@@ -0,0 +1,33 @@
+var dataCacheName = 'tzappData-v1';
+var cacheName = 'tzapp-1';
+var filesToCache = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {e.respondWith(fetch(e.request));});
