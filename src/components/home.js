6970abe58615b67518e842ac449c29de90f73acd
Maxwell <maxwellsouzacarvalho@gmail.com>
2018-04-03 03:22:51 -0300

    update test release

--- src/components/home.js
+++ src/components/home.js
@@ -10,16 +10,16 @@ export default class componentName extends Component {
                 <Header />
                 <List component="nav">
                     <ListItem button component={Link} to="/items">
-                        <ListItemText primary="Items" secondary="Items details (potion, phoenix down, etc)" />
+                        <ListItemText primary="Items" secondary="Items details (potion, phoenix down, etc)." />
                     </ListItem>
                     <ListItem button component={Link} to="/bazaar">
-                        <ListItemText primary="Bazaar" secondary="Bazaar recipes and details" />
+                        <ListItemText primary="Bazaar" secondary="Bazaar recipes and details." />
                     </ListItem>
                     <ListItem button component={Link} to="/hunts">
-                        <ListItemText primary="Hunts" secondary="Hunts instructions, rewards and tips" />
+                        <ListItemText primary="Hunts" secondary="Hunts instructions, rewards and tips." />
                     </ListItem>
                     <ListItem button component={Link} to="/bestiary">
-                        <ListItemText primary="Bestiary" secondary="Details about monsters" />
+                        <ListItemText primary="Bestiary" secondary="Details about monsters." />
                     </ListItem>
                 </List>
             </Fragment>
